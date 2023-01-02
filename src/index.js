2267592c8a04005ea93a62a335f6102417c36764
TrietB <triet.bm1996@gmail.com>
2023-01-02 17:15:47 +0700

    add paragraph test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './App'
+import Paragraphs from './Paragraphs'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
+    <>
     <App/>
+    <Paragraphs/>
+    </>
 )
